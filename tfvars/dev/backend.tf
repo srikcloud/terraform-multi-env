1e5475bacc6bf6cbc92792489628a1323a694bdc
srikanth <srikanth.baireddy@outlook.com>
2025-07-04 11:20:56 +0530

    terraform

---
+++ tfvars/dev/backend.tf
@@ -0,0 +1,6 @@
+
+bucket = "srikcloud-remote-state-dev"
+    key    = "tfvars-demo"
+    region = "us-east-1"
+    encrypt = true
+    use_lockfile = true
---
+++ tfvars/prod/backend.tf
@@ -0,0 +1,6 @@
+
+bucket = "srikcloud-remote-state-prod"
+    key    = "tfvars-demo"
+    region = "us-east-1"
+    encrypt = true
+    use_lockfile = true
