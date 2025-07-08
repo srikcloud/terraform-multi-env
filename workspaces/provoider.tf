905f9c8fa67dc1fd5c6342d63b1481d593583913
srikanth <srikanth.baireddy@outlook.com>
2025-07-08 22:27:27 +0530

    terraform

---
+++ workspaces/provoider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws= {
+      source = "hashicorp/aws"
+      version = "5.98.0" 
+  }
+}
+ backend "s3" {
+    bucket = "srikcloud-remote-state"
+    key    = "workspace-demo"
+    region = "us-east-1"
+    encrypt = true
+    use_lockfile = true
+  }
+}
+provider "aws" {
+  #configuration options
+  }
